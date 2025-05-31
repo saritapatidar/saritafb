37d4b9e4d4447d7e86d279ff08eb8f46f0e235a3
saritapatidar <saritapatidar@thoughtwin.com>
2025-05-31 21:21:17 +0530

    Update views.py

--- facebookclone/apps/fb/views.py
+++ facebookclone/apps/fb/views.py
@@ -154,7 +154,28 @@ class commentmodelviewset(viewsets.ModelViewSet):
 # from rest_framework.authentication import BasicAuthentication
 
 
+# custompermissions.py
 
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Allow users to edit/delete only their own objects.
+    """
+
+    def has_permission(self, request, view):
+        # Allow only authenticated users
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Handle different models (CustomUser vs Profile vs Post)
+        if hasattr(obj, 'user'):  # Post, Comment, Profile
+            return obj.user == request.user or getattr(obj.user, 'user', None) == request.user
+        return obj == request.user  # For CustomUser model
+# or getattr(obj.user, 'user', None) == request.user
 
 
 # def login_page(request):
