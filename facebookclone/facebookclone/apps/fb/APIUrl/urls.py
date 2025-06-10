637bf1befe0341084683054cc16ab68d70fac1f1
sarita <saritapatidar@thoughtwin.com>
2025-06-10 18:29:21 +0530

    firstcommit

--- facebookclone/facebookclone/apps/fb/APIUrl/urls.py
+++ facebookclone/facebookclone/apps/fb/APIUrl/urls.py
@@ -1,22 +1,24 @@
 from rest_framework.routers import DefaultRouter
-from .import Apiviews
+from fb.APIViews.views import Usermodelviewset,UsermodelviewsetRUD
 from django.urls import path,include
-from .Apiviews import Login, LogoutAPI,UserRegistrationView
+from fb.APIViews.views import Login, LogoutAPI,UserRegistrationView
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
-
+from fb.APIViews import views
 
 router=DefaultRouter()
 
-router.register('userapi',Apiviews.usermodelviewset,basename='user')
-router.register('postapi',Apiviews.postmodelviewset,basename='post')
-router.register('commentapi',Apiviews.commentmodelviewset,basename='comment')
+# router.register('userapi',Apiviews.usermodelviewset,basename='user')
+router.register('postapi',views.Postmodelviewset,basename='post')
+router.register('commentapi',views.Commentmodelviewset,basename='comment')
 # router.register('parent',views.parentmodelviewset,basename='parent')
 
 urlpatterns = [
 
                 path('',include(router.urls)),
                 path('api-auth/',include('rest_framework.urls')),
+                path('userapi/',Usermodelviewset.as_view(),name='userapi'),
+                path('userapi/<int:pk>/',UsermodelviewsetRUD.as_view(),name='userapi'),
                 path('registration/', UserRegistrationView.as_view(), name='registration'),
                 path('loginapi/', Login.as_view(), name='loginapi'),
                 path('logoutapi/', LogoutAPI.as_view(), name='logoutapi'),
