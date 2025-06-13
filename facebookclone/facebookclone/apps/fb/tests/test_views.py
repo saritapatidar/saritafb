8359ea7c618c72f8176ac04ff580bdf089814a39
sarita <saritapatidar@thoughtwin.com>
2025-06-13 17:26:46 +0530

    first commit

--- facebookclone/facebookclone/apps/fb/tests/test_views.py
+++ facebookclone/facebookclone/apps/fb/tests/test_views.py
@@ -9,140 +9,147 @@ from django.contrib.auth import get_user_model
 from django.core import mail
 from django.contrib.auth.hashers import check_password
 
-User = get_user_model()
-
-class AuthViewsTest(TestCase):
+class SocialMediaViewTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.signup_url = reverse('signup') 
+        self.user1 = CustomUser.objects.create_user(firstname='sonam',lastname='patidar',Date_of_birth="2000-09-12",gender="female",email="sonam@gmail.com",phone_number='8827489124', password='Asdf@123')
+        self.user2 = CustomUser.objects.create_user(firstname='rahul',lastname='patidar',Date_of_birth="2000-08-23",gender="male",email="Rahul@gmail.com",phone_number='8827489136', password='Asdf@123')
+        self.user1_profile = UserProfile.objects.create(user=self.user1)
+        self.user2_profile = UserProfile.objects.create(user=self.user2)
         self.login_url = reverse('login')
-        self.logout_url = reverse('logout')
         self.home_url = reverse('home')
-
-        self.user_data = {
-            'firstname': 'John',
-            'lastname': 'Doe',
-            'Date_of_birth':'2000-04-12',
-            'gender':'Male',
-            'email': 'johndoe@gmail.com',
-            'phone_number': '9981979637',
-            'password': 'Asdf@123'
-        }
-
-    def test_signup_get(self):
-        response = self.client.get(self.signup_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'fb/signup.html')
-
-
-
-    # def test_signup_post_valid(self):
-    #     response = self.client.post(self.signup_url,self.user_data)
-    #     self.assertRedirects(response, self.login_url)
-
-        # self.assertEqual(User.objects.count(), 1)
-        # user = User.objects.first()
-        # self.assertEqual(user.email, self.user_data['email'])
-        # self.assertTrue(check_password(self.user_data['password'], user.password))
-
-    def test_signup_post_invalid(self):
-    	invalid_data=self.user_data.copy()
-    	invalid_data['email']=''
-    	response=self.client.post(self.signup_url,invalid_data)
-    	self.assertEqual(response.status_code,200)
-    	self.assertFormError(response,'form','email','This field is required.')
-
-    def test_login_get(self):
-    	response=self.client.get(self.login_url)
-    	self.assertEqual(response.status_code,200)
-    	self.assertTemplateUsed(response,'fb/login.html')
-
-    def test_login_post_valid(self):
-    	user=User.objects.create_user(phone_number=self.user_data['phone_number'],password=self.user_data['password'])
-    	response=self.client.post(self.login_url,{'phone_number':self.user_data['phone_number'],'password':self.user_data['password']})
-    	self.assertRedirects(response,self.home_url)
-    	self.assertTrue('_auth_user_id' in self.client.session)
-    
-    def test_login_post_invalid(self):
-    	response=self.client.post(self.login_url,{'phone_number':'11111111','password':'tttt'})
-    	self.assertEqual(response.status_code,200)
-    	self.assertFormError(response,'form',None,'Invalid')
-
-
-    def test_logout(self):
-    	user=User.objects.create_user(phone_number=self.user_data['phone_number'],password=self.user_data['password'])
-    	self.client.login(phone_number=self.user_data['phone_number'], password=self.user_data['password'])
-    	response=self.client.get(self.logout_url)
-    	self.assertRedirects(response,self.login_url)
-
-
-class HomePageViewTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(firstname='Arpita', password='Asdf@123')
-        self.profile = UserProfile.objects.create(user=self.user)
-        self.home_url = reverse('home')
-    
-    def test_home_page_get_authenticated(self):
         self.client.login(phone_number='8827489124', password='Asdf@123')
+
+    def test_home_page_get(self):
         response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_create_post(self):
+        response = self.client.post(self.home_url, {'content': 'Test post'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CreatePost.objects.filter(content='Test post').exists())
+
+    def test_signup_view(self):
+        response = self.client.post(reverse('signup'), {
+            'firstname': 'kajal',
+            'lastname': 'patidar',
+            'email': 'kajal12@example.com',
+            'phone_number': '8827489120',
+            'password': 'Asdf@123',
+            'Date_of_birth': '2000-01-01'
+        })
+        self.assertEqual(response.status_code, 200 or 302)
+
+    def test_login_view(self):
+        self.client.logout()
+        response = self.client.post(self.login_url, {'phone_number': '8827489123', 'password': 'Asdf@123'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_view(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_profile_view(self):
+        url = reverse('profile', args=[self.user2.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'fb/profile.html')
+
+    def test_like_view(self):
+        post = CreatePost.objects.create(user=self.user2_profile, content='Like this')
+        response = self.client.post(reverse('likepost', args=[post.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.user1, post.likes.all())
+
+    def test_comment_view(self):
+        post = CreatePost.objects.create(user=self.user2_profile, content='Post')
+        response = self.client.post(reverse('commen', args=[post.id]), {
+            'text': 'Nice post!'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Comment.objects.filter(text='Nice post!').exists())
+
+    def test_send_friend_request(self):
+        response=self.client.get(reverse('send_friend_request',args=[self.user2.id]))
         self.assertEqual(response.status_code,302)
-        # self.assertTemplateUsed(response, 'home.html')
-        # self.assertIn('posts', response.context)
-        # self.assertIn('users', response.context)
+        self.assertTrue(FriendRequest.objects.filter(from_user=self.user1,to_user=self.user2).exists())
+
+    def test_accept_friend_request(self):
+        friend_request=FriendRequest.objects.create(from_user=self.user2,to_user=self.user1)
+        response=self.client.get(reverse('accept_friend_request',args=[friend_request.id]))
+        self.assertEqual(response.status_code,302)
+        self.assertFalse(FriendRequest.objects.filter(id=friend_request.id).exists())
+        self.assertTrue(Follow.objects.filter(follower=self.user1,followed=self.user2).exists())
+    
+    def test_show_friend_request(self):
+        response=self.client.get(reverse('show_friends',args=[self.user1.id]))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'send_request.html')
+
+    def test_followers_list(self):
+        response=self.client.get(reverse('followers_list',args=[self.user1.id]))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'followers_list.html')
 
 
+    def test_following_list(self):
+        response=self.client.get(reverse('following_list',args=[self.user1.id]))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'following_list.html')
 
 
+    def test_edit_profile(self):
+        response=self.client.get(reverse('edit_profiles'))
+        self.assertEqual(response.status_code,200)
+        self.assertTrue(response,'edit_profile.html')
+
+    def test_edit_profile_post(self):
+        response=self.client.post(reverse('edit_profiles'),{'bio':'Updated bio'})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(UserProfile.objects.get(user=self.user1).bio,'Updated bio')
+
+    def test_userpost(self):
+        response=self.client.get(reverse('user_posts'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'user_posts.html')
 
 
+    def test_Deletepost(self):
+        post=CreatePost.objects.create(user=self.user1_profile,content="Deleted")
+        response=self.client.post('delete_post',args=[post.id])
+        self.assertEqual(response.status_code,404)
+        self.assertTrue(CreatePost.objects.filter(id=post.id).exists())
 
+    
+    def text_show_comment_get(self):
+        post=CreatePost.objects.create(user=self.user1_profile,content="comment here")
+        response=self.client.get('morecomment',args=[post.id])
+        self.assertEqual(response.status_code,200)
 
+    def test_show_comment_post(self):
+        post=CreatePost.objects.create(user=self.user1_profile,content="comment")
+        response=self.client.post(reverse('morecomment',args=[post.id]),{'text':'Another comment'})
+        self.assertEqual(response.status_code,302)
+        self.assertTrue(Comment.objects.filter(text='Another comment').exists())
 
+        
 
+  
 
+    
+   
 
+  
 
+    
 
+    
 
-
-
-
-
-
-
-
-#     def test_home_page_get_authenticated(self):
-#         self.client.login(username='testuser', password='pass')
-#         response = self.client.get(self.home_url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'home.html')
-#         self.assertIn('posts', response.context)
-#         self.assertIn('users', response.context)
-
-#     def test_home_page_redirect_if_not_logged_in(self):
-#         response = self.client.get(self.home_url)
-#         self.assertNotEqual(response.status_code, 200)
-#         self.assertRedirects(response, f'/accounts/login/?next={self.home_url}')
-
-#     def test_home_page_post_create_text_post(self):
-#         self.client.login(username='testuser', password='pass')
-#         response = self.client.post(self.home_url, {'content': 'My first post'})
-#         self.assertEqual(response.status_code, 302)
-#         self.assertTrue(CreatePost.objects.filter(user=self.profile, content='My first post').exists())
-
-#     def test_home_page_post_create_image_post(self):
-#         self.client.login(username='testuser', password='pass')
-#         image = SimpleUploadedFile("test.jpg", b"file_content", content_type="image/jpeg")
-#         response = self.client.post(self.home_url, {'image': image})
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(CreatePost.objects.count(), 1)
-#         self.assertIsNotNone(CreatePost.objects.first().image)
-
-#     def test_home_page_post_empty_content_image(self):
-#         self.client.login(username='testuser', password='pass')
-#         response = self.client.post(self.home_url, {'content': '', 'image': ''})
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(CreatePost.objects.count(), 0)
-
-
+    # def test_post_comment_from_show_page(self):
+    #     post = CreatePost.objects.create(user=self.user1_profile, content='Comment here')
+    #     response = self.client.post(reverse('show_comments', args=[post.id]), {
+    #         'content': 'Another comment'
+    #     })
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertTrue(Comment.objects.filter(content='Another comment').exists())
